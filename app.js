8f1d307f71f85c55276001adaddfa7ec27ef59df
Qazal Jalilian <qazal.jalilian@palettesoftware.com>
2021-11-16 14:24:06 +0100

    pl

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ const notificationPayload = {
           "title": "Go to the site"
       }],
       "data": {
-        "url": "https://pwa-backend-qazal.herokuapp.com",
+        "url": "https://qazaljalilian.github.io/",
         "favorite_count": 0,
         "retweet_count": 0
       }
